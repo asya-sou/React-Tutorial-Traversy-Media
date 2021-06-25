0b5914de2e95de3efd0a2f5240d09b2cc9339fcf
asya-sou <asyasoughou@gmail.com>
2021-06-25 19:11:00 -0400

    addTask

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,8 @@
 import {useState} from 'react'
 import Header from './components/Header'
 import Tasks from './components/Tasks'
+import AddTask from './components/AddTask'
+
 /* import React from 'react'
  */
 
@@ -19,7 +21,14 @@ function App() {
         reminder: false,
     },
 ])
+  //add task
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) + 1
+    const newTask = { id, ...task}
+    setTasks ([...tasks, newTask])
+  } 
 
+  //delete
   const deleteTask = (id) => {
     setTasks(tasks.filter((task) => task.id !== id))
   }
@@ -32,6 +41,7 @@ function App() {
   return (
     <div className="container">
       <Header /> 
+      <AddTask onAdd={addTask} />
       {tasks.length > 0 ? 
       <Tasks tasks={tasks} 
       onDelete={deleteTask} 
